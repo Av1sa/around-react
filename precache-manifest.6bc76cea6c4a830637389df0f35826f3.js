20e4c03c7a4f005625171d59f686c3abda97bbc7
Avisa <manelis1102@gmail.com>
2020-09-24 01:20:34 +0300

    Updates

---
+++ precache-manifest.6bc76cea6c4a830637389df0f35826f3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76fdb31d0dd384246d0b864fefbd4fc3",
+    "url": "/Av1sa/around-react/index.html"
+  },
+  {
+    "revision": "db6a0bbd9463f409038b",
+    "url": "/Av1sa/around-react/static/css/main.e83bc941.chunk.css"
+  },
+  {
+    "revision": "6e3663d744b8dad36ed0",
+    "url": "/Av1sa/around-react/static/js/2.07483834.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Av1sa/around-react/static/js/2.07483834.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db6a0bbd9463f409038b",
+    "url": "/Av1sa/around-react/static/js/main.bd91e11c.chunk.js"
+  },
+  {
+    "revision": "9c281310c3de4ae1895a",
+    "url": "/Av1sa/around-react/static/js/runtime-main.f75a5d5e.js"
+  },
+  {
+    "revision": "001893789f7f342b520f29ac8af7d6ca",
+    "url": "/Av1sa/around-react/static/media/Inter-Bold.00189378.woff"
+  },
+  {
+    "revision": "3ae6a7d3890c33d857fc00bd2e4c4820",
+    "url": "/Av1sa/around-react/static/media/Inter-Regular.3ae6a7d3.woff"
+  },
+  {
+    "revision": "5df32a06fdf69f3ae19600017c36d45a",
+    "url": "/Av1sa/around-react/static/media/close_icon.5df32a06.svg"
+  },
+  {
+    "revision": "4220563aa7e7f6bb1d562a95174678b6",
+    "url": "/Av1sa/around-react/static/media/delete_icon.4220563a.svg"
+  },
+  {
+    "revision": "6bfdb35467f51d3ba1f70f8d4a9c2b59",
+    "url": "/Av1sa/around-react/static/media/edit_icon.6bfdb354.svg"
+  },
+  {
+    "revision": "d476a8b86d0b656cfa463c1148e96069",
+    "url": "/Av1sa/around-react/static/media/edit_large_icon.d476a8b8.svg"
+  },
+  {
+    "revision": "5bbb05f725aa334a73acf3174e255f4e",
+    "url": "/Av1sa/around-react/static/media/like-black_icon.5bbb05f7.svg"
+  },
+  {
+    "revision": "a8acc871456b1001597672e02c5199ca",
+    "url": "/Av1sa/around-react/static/media/like_icon.a8acc871.svg"
+  },
+  {
+    "revision": "8f7611ae39c682f37516feec261f77dd",
+    "url": "/Av1sa/around-react/static/media/logo.8f7611ae.svg"
+  },
+  {
+    "revision": "b2fce4ac45f3d116cd48ec8d068ab723",
+    "url": "/Av1sa/around-react/static/media/plus_icon.b2fce4ac.svg"
+  }
+]);
